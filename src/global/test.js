c927f1caf6a031e2157b058a7f8672edb0dc6363
jiffang <jiffang@ebay.com>
2017-04-09 19:32:10 +0800

    Add code

--- src/global/test.js
+++ src/global/test.js
@@ -6,3 +6,11 @@ var testModule = require('./testModule');
 if(module === require.main) {
   console.log("This is the main module in test.js");
 }
+
+require.resolve('./testModule');
+
+console.log(require.cache);
+
+console.log(__dirname);
+
+console.log(__filename);
